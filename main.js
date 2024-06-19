93c3487dcbd0c2893c6384775cb830ce01cf2505
tibebu <onetwothreetibe@gmail.com>
2024-06-19 03:48:51 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const navalist = document.getElementById('navalist');
+const ict = document .getElementById('ict');
+
+//click event
+ict.addEventListener('click', ()=>{
+    navalist.classList.toggle('navalist-active')
+})
